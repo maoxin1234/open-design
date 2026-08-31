c1eea12c4e665d264118857406b8479cfd912544
maoxin1234 <875408344@qq.com>
2026-08-31 20:43:35 +0800

    fix(chat): give prompt_too_large card a New conversation CTA (#4782)

--- apps/web/src/i18n/types.ts
+++ apps/web/src/i18n/types.ts
@@ -2870,6 +2870,7 @@ export interface Dict {
   'chat.runError.title.artifactMissing': string;
   'chat.runError.signInMessage.amr': string;
   'chat.runError.signInMessage.other': string;
+  'chat.runError.reduceContextCta': string;
   'chat.runError.cliMissingMessage': string;
   'chat.runError.promptTooLargeMessage': string;
   'chat.runError.modelUnavailableMessage': string;
